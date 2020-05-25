7653876b3f11898aed4300afc1e6ad36e42d81b2
Yash Khandelwal <yashkhandelwal2017@gmail.com>
2020-05-26 02:23:14 +0530

    first commit

---
+++ mymodel.py
@@ -0,0 +1,100 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.datasets import fashion_mnist
+from keras.utils import np_utils
+import keras
+import sys
+from keras.utils.vis_utils import plot_model
+sys.stdin=open('input.txt','r')
+(x_train, y_train), (x_test, y_test)  = fashion_mnist.load_data()
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
+model = Sequential()
+convlayers = int(sys.stdin.readline())
+first_layer_nfilter = int(sys.stdin.readline())
+first_layer_kernel_size = int(sys.stdin.readline())
+first_layer_pool_size = int(sys.stdin.readline())
+model.add(Conv2D(first_layer_nfilter, (first_layer_kernel_size, first_layer_kernel_size),
+                 padding = "same", 
+                 input_shape = input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (first_layer_pool_size, first_layer_pool_size)))
+for i in range(1,convlayers):
+	nfilters = int(sys.stdin.readline())
+	kernel_size = int(sys.stdin.readline())
+	pool_size = int(sys.stdin.readline())
+	model.add(Conv2D(nfilters, (kernel_size, kernel_size),padding = "same"))
+	model.add(Activation("relu"))
+	model.add(MaxPooling2D(pool_size = (pool_size, pool_size)))
+model.add(Flatten())
+fc_input = int(sys.stdin.readline())
+for i in range(0,fc_input):
+	no_neurons = int(sys.stdin.readline())
+	model.add(Dense(no_neurons))
+	model.add(Activation("relu"))
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+print(model.summary())
+batch_size = 64
+epochs = 5
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+model.save("fashion-mnist.h5")
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+accuracy_file = open('accuracy.txt','w')
+accuracy_file.write(str(scores[1]))
+accuracy_file.close()
+plot_model(model, to_file='fashion-mnist.png', show_shapes=True, show_layer_names=True)
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+host_address = "khandelwalyash6899@gmail.com"
+host_pass = "012345aB"
+guest_address = "yashkhandelwal2017@gmail.com"
+subject = "Regarding Info of your model "
+content = '''Hello, 
+				Developer this is an email regarding your model.
+				Congratulations on your success.
+			THANK YOU ...'''+'\n Its accuracy is  '+ str(scores[1])
+message = MIMEMultipart()
+message['From'] = host_address
+message['To'] = guest_address
+message['Subject'] = subject
+message.attach(MIMEText(content, 'plain'))
+fp = open('fashion-mnist.png', 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+msgImage.add_header('Content-ID', '<image1>')
+message.attach(msgImage)
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.starttls()
+session.login(host_address, host_pass)
+text = message.as_string()
+session.sendmail(host_address, guest_address  , text)
+session.quit()
+print('Successfully sent your mail')
---
+++ tweak.py
@@ -0,0 +1,98 @@
+data_file = open('data.txt','r')
+input_file = open('input.txt','r')
+accuracy_file = open('accuracy.txt','r')
+
+data = data_file.read()
+data = data.split('\n')
+old_accuracy = float(data[0])		  
+layer = int(data[1])		                     
+line = int(data[2])		                    
+cp_line = line % 3			  
+entered_data = int(data[3])		  
+old_data = int(data[4])		 
+index_fc = int(data[5])		  
+new_accuracy = float(accuracy_file.read()) 			  
+inputs = input_file.read()
+inputs = inputs.split('\n')
+if new_accuracy > old_accuracy and new_accuracy - old_accuracy >= 0.00001 :
+	old_accuracy = new_accuracy
+	if layer == 1:
+		if cp_line == 1:
+			entered_data = entered_data * 2
+		else :
+			entered_data += 1	 
+	else :
+		entered_data += 100		 
+	inputs[line] = str(entered_data)	  							
+
+else:
+	
+	if layer == 1 :							
+		if cp_line == 1 :						
+
+			if entered_data//2 == old_data :
+
+				inputs = inputs[0:line]
+				inputs.append('1') 				
+				layer = 2					
+				index_fc = line    				
+				inputs.append('64')				
+				old_data = 64				
+				entered_data = 64				
+				line = line + 1				
+				inputs[0] = str(int(inputs[0]) - 1)		
+				
+				inputs[line] = str(entered_data//2)	
+				line = line + 1				
+				entered_data = 3			
+				old_data = 2				
+				inputs[line] = str(entered_data)	
+		elif cp_line == 2:
+
+			
+			inputs[line] = str(entered_data - 1)		
+			line = line + 1				
+			entered_data = 3				
+			old_data = 2				
+			inputs[line] = str(entered_data)	 
+		
+		elif cp_line == 0:
+
+			inputs[line] = str(entered_data - 1)		
+			line = line + 1				
+			old_data = int(inputs[line - 3])			
+			entered_data = old_data * 2			 
+			inputs[0] = str(int(inputs[0]) + 1) 		
+			inputs = inputs[0:line]			
+			inputs.append(str(entered_data))		
+			inputs.append('2')				
+	else:
+		
+
+		noOfLayers = int(inputs[index_fc])+1		                  
+		inputs[index_fc]=str(noOfLayers)			
+		entered_data -= 64					
+		old_data = entered_data				
+		inputs[line] = str(entered_data)			
+		line += 1						
+		inputs.append(str(entered_data))			 
+
+
+data_file.close()
+input_file.close()
+
+
+data_file = open('data.txt','w')
+input_file = open('input.txt','w')
+
+data_file_data = str(old_accuracy) + '\n' + str(layer) + '\n' + str(line) + '\n' + str(entered_data) + '\n' + str(old_data) + '\n' + str(index_fc)
+
+data_file.write(data_file_data)						
+
+data_file.close()
+
+input_file_data = '\n'.join(inputs)
+
+input_file.write(input_file_data)					
+
+input_file.close()
